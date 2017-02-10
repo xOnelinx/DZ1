dd01064502ab2482612ae3856d8cd65f98ffe487
xOnelinx <Onelin@ya.ru>
2017-02-10 17:59:23 +0300

    новый класс к 6му уроку

---
+++ src/DZ/Lvl1/Evseev_Denis_Lesson_6.java
@@ -0,0 +1,11 @@
+package DZ.Lvl1;
+
+/**
+ * Created by DENIS on 10.02.2017.
+ */
+public class Evseev_Denis_Lesson_6 {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
