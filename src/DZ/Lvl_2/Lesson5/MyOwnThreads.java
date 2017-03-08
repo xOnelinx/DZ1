4d78a0b5fa67ffb3d65f115206513f5a577ed69e
xOnelinx <Onelin@ya.ru>
2017-03-08 19:38:59 +0300

    готова домашка 5 теперь я иду спать

--- src/DZ/Lvl_2/Lesson5/MyOwnThreads.java
+++ src/DZ/Lvl_2/Lesson5/MyOwnThreads.java
@@ -11,7 +11,7 @@ public class MyOwnThreads {
         for (int i = 0; i < size; i++) {
             arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
         }
-        System.out.print("time - ");
+        System.out.print("время 1 поток - ");
         System.out.println(System.currentTimeMillis()-a);
     }
 
